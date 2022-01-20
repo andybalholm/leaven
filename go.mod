b84a6adfce46c044e44f562508a2440d8fe0448f
Andy Balholm <andy@balholm.com>
2022-01-19 21:29:50 -0800

    Update llvm package to get use-tracking API.

--- go.mod
+++ go.mod
@@ -3,12 +3,14 @@ module github.com/andybalholm/leaven
 go 1.18
 
 require (
-	github.com/llir/llvm v0.3.4
+	github.com/llir/llvm v0.3.5-0.20220120022543-ad080cea55b7
 	golang.org/x/sys v0.0.0-20210510120138-977fb7262007
 )
 
 require (
 	github.com/elliotchance/c2go v0.26.10 // indirect
+	github.com/kr/pretty v0.2.0 // indirect
+	github.com/kr/text v0.2.0 // indirect
 	github.com/llir/ll v0.0.0-20210719001141-246f2b6b1fa9 // indirect
 	github.com/mewmew/float v0.0.0-20201204173432-505706aa38fa // indirect
 	github.com/pkg/errors v0.9.1 // indirect
--- testdata/main.go
+++ testdata/main.go
@@ -1,7 +1,30 @@
 package main
 
-import "os"
+import (
+	"flag"
+	"log"
+	"os"
+	"runtime/pprof"
+)
+
+var cpuprofile = flag.String("cpuprofile", "", "file to write CPU profile to")
 
 func main() {
-	os.Exit(int(test_main()))
+	flag.Parse()
+	if *cpuprofile != "" {
+		f, err := os.Create(*cpuprofile)
+		if err != nil {
+			log.Fatal("could not create CPU profile: ", err)
+		}
+		defer f.Close() // error handling omitted for example
+		if err := pprof.StartCPUProfile(f); err != nil {
+			log.Fatal("could not start CPU profile: ", err)
+		}
+		defer pprof.StopCPUProfile()
+	}
+
+	ret := int(test_main())
+	if ret != 0 {
+		os.Exit(ret)
+	}
 }
